704bfbc7700f82c8094c698f01598e5468650b98
Yankun Wang <46727894+Yankun-Wang@users.noreply.github.com>
2019-04-18 18:36:09 -0400

    Add files via upload

--- js/filter.js
+++ js/filter.js
@@ -1,21 +1,29 @@
 filterSelection("all")
+
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
+  // x = all the elements in the filter div
   if (c == "all") c = "";
+
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  //i++ for loop
   for (i = 0; i < x.length; i++) {
+    //remove all the "show" class
     RemoveClass(x[i], "show");
+    //indexOf()Method: returns -1 if the value to search for never occurs. If the className = btn class name, add "show" class
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
+// Show filtered elements, name = "show"
 function AddClass(element, name) {
   var i, arr1, arr2;
+//arr=number group
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
+    //if this class not exist (=-1), add it to the element
     if (arr1.indexOf(arr2[i]) == -1) {
       element.className += " " + arr2[i];
     }
